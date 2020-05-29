760f280a671173133093c7bb68f216a0ea3d44e4
unknown <akhilesharora002@gmail.com>
2020-05-28 20:07:59 -0400

    My First Commit

---
+++ LAb1.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var http = require('http');
+http.createServer(function (req, res) {
+    res.writeHead(200, {
+        "Content-Type": "text/plain"
+    });
+    res.end('hello World!!');
+}).listen(3000);
+
+
+console.log('Hello world port 3000');
